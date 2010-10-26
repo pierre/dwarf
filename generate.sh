9440b68fe4445260bca42fc25bb9f9ff2798f97b
Pierre-Alexandre Meyer <pierre@ning.com>
2010-10-26 16:16:26 -0700

    resources: add collector Hadoop flow
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- generate.sh
+++ generate.sh
@@ -1,3 +1,5 @@
+dot resources/collector_disk_buckets.dot -Tpng -o resources/collector_disk_buckets.png
+dot resources/collector_hadoop_flow.dot -Tpng -o resources/collector_hadoop_flow.png
 dot resources/collector_event_flow.dot -Tpng -o resources/collector_event_flow.png
 dot resources/overview.dot -Tpng -o resources/overview.png
 jekyll --pygments --safe --server
